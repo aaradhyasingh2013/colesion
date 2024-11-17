3a071f7698f8951f78d7437e5da10afb1b7361d2
aaradhyasingh2013 <kuki81kumari@gmail.com>
2024-11-17 17:40:32 +0530

    colesion

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+import random
+screen_width,  screen_height= 500,400
+movement_speed= 5
+font_size= 72
+pygame.init()
+background_image= pygame.transform.scale(pygame.image.load("bg.jpg")(screen_width, screen_hight))
+font= pygame.font.SysFont("algerian",font_size)
+class sprite(pygame.sprite.Sprite):
+    def __init __()
+    self.image= pygame.
